daa349b49e8e73fe50592a9d62d8c4ed7116cf01
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-10-16 17:11:04 +0100

    Helper function to cleanly get carousel pictures. asset tweak, centering project pages

--- src/pages/Flashcards.jsx
+++ src/pages/Flashcards.jsx
@@ -1,18 +1,15 @@
 import Carousel from "../components/carousel"
 import BigLink from "../components/biglink"
-import image from "../assets/image.png"
-import image2 from "../assets/image2.png"
-import image3 from "../assets/image3.png"
-
+import getImages from "../helper/getImages"
 
 export default function Flashcards() {
-    const imgs = [image, image2, image3]
+    const imgs = getImages("flash", 6)
     return (
         <>
-            <div className="container-xl">
+            <div className="container-xl center">
                 <h1>Flashcards</h1>
                 <p>A flashcards web app built with react. Featuring multiple choice questions, stats, saving and loading flashcards, a minimal, responsive design and more.</p>
-                <Carousel imgs={imgs} arrowColour = "white"/>
+                <Carousel imgs={imgs} arrowColour="white" />
                 <BigLink href="https://j-wll.github.io/flashcards-pages/" text="Try it here" />
                 <BigLink href="https://github.com/J-Wll/flashcards" text="GitHub Repo" />
             </div>
