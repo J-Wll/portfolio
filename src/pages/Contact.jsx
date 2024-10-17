e852219b544df54ac9919247ec024d83805eb529
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-10-17 19:54:34 +0100

    CSS margin and contact content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,12 +3,14 @@ import BigLink from "../components/biglink"
 export default function Contact() {
     return (
         <div className="container-xl">
-
-            <h1>Please contact me via Email or LinkedIn.</h1>
-            <h3>Email me at <a href="mailto:westwellWork@gmail.com">westwellWork@gmail.com</a></h3>
-            <BigLink text = "My LinkedIn"/>
-            <BigLink text = "My GitHub"/>
-            <BigLink text = "My CV"/>
+            <h1>Contact</h1>
+            <p>I'm currently seeking employment and am open to a range of software development work. I hope the projects here and on my GitHub demonstrate knowledge and skill with a variety of languages and types of programming.    </p>
+            <p>Please contact me via Email or LinkedIn.</p>
+            <BigLink href="mailto:westwellWork@gmail.com" text="Email: westwellWork@gmail.com"/>
+            <BigLink href="https://www.linkedin.com/in/joe-westwell/" text = "My LinkedIn"/>
+            <BigLink href="https://github.com/J-Wll" text = "My GitHub"/>
+            {/* TODO UPDATE TO FINAL CV */}
+            <BigLink href="https://raw.githubusercontent.com/J-Wll/portfolio/refs/heads/main/src/assets/cv.webp?token=GHSAT0AAAAAACUV5SPK25HAQASID45ETKV4ZYRLBNA" text = "My CV"/>
         </div>
 
     )
